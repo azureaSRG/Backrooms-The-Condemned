fa586a239ba8ed5a72dae5e3c2784a4d2814288a
DesdeDerrota <270583@pcti.mobi>
2026-06-09 22:33:35 -0400

    Implement Hound class with basic structure

--- Java Project/core/src/main/java/com/github/azereaSRG/capstone/EntityScripts/Hound.java
+++ Java Project/core/src/main/java/com/github/azereaSRG/capstone/EntityScripts/Hound.java
@@ -1,3 +1,28 @@
 public Class Hound implements EnemyInterface {
-  
+  boolean isFrozen; // protected
+  EntityStates state = EntityStates.NEUTRAL; // protected, default setting
+  ActionStates action = ActionStates.NEUTRAL; // protected, default setting
+
+  public Hound() {
+      // to be implemented
+  }
+
+    @override
+    public void attack() {
+        
+    }
+
+    @override
+    public void wander() {
+        
+    }
+
+    @override
+    public void flee() {
+        
+    }
+
+    public EntityClass spawn(Viewpoint viewpoint, Texture texture, Player player) {
+        
+    }
 }

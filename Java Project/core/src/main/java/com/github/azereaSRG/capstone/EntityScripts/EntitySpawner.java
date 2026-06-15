65694aecb27b14180418443fc67dc78c7ec27e67
azureaSRG <162836474+azureaSRG@users.noreply.github.com>
2026-06-15 14:43:38 -0400

    Spawner

--- Java Project/core/src/main/java/com/github/azereaSRG/capstone/EntityScripts/EntitySpawner.java
+++ Java Project/core/src/main/java/com/github/azereaSRG/capstone/EntityScripts/EntitySpawner.java
@@ -1,7 +1,10 @@
 package com.github.azereaSRG.capstone.EntityScripts;
 
+import java.util.ArrayList;
+
 public class EntitySpawner {
     public static final int ENTITY_LIMIT = 70;
+    private static ArrayList<EntityInterface.EntityClass> entities = new ArrayList<>();
 
     public static void spawnEntity(EntityInterface.EntityClass entity) {
         

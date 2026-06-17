ec6d22c5b0f06124334b45f93893ebb5ad5ccce9
azureaSRG <162836474+azureaSRG@users.noreply.github.com>
2026-06-17 10:57:59 -0400

    Updated Spawner

--- Java Project/core/src/main/java/com/github/azereaSRG/capstone/entityutil/EntitySpawner.java
+++ Java Project/core/src/main/java/com/github/azereaSRG/capstone/entityutil/EntitySpawner.java
@@ -7,10 +7,15 @@ public class EntitySpawner {
     private static ArrayList<EntityInterface.EntityClass> entities = new ArrayList<>();
 
     public static void spawnEntity(EntityInterface.EntityClass entity) {
-
+        spawnEntity(entity, 0, 0);
     }
 
     public static void spawnEntity(EntityInterface.EntityClass entity, float x, float y) {
+        if (entities.size() + 1 > ENTITY_LIMIT) {
+            System.out.println("Entity could not be summoned because the entity cap has been reached");
+            return;
+        }
 
+        entities.add(new entity.spawn(x, y));
     }
 }
